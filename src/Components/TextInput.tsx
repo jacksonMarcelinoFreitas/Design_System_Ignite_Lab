9f7b08255d17a70253256aa1f011ccea29955c71
Jackson Freitas <jacksonzitap.mc@gmail.com>
2022-10-13 18:12:58 -0300

    first commit

---
+++ src/Components/TextInput.tsx
@@ -0,0 +1,50 @@
+import { Slot } from '@radix-ui/react-slot'
+import { InputHTMLAttributes, ReactNode } from 'react';
+
+
+
+export interface TextInputRootProps{
+    children: ReactNode;
+}
+
+function TextInputRoot(props: TextInputRootProps){
+return (
+    <div className = "flex items-center gap-3 py-4 h-12 px-3 rounded bg-gray-800 w-full focus-whithin:ring-2 ring-cyan-300">
+        {props.children}
+    </div>
+)
+}
+
+export interface TextInputIconProps{
+    children: ReactNode;
+}
+
+function TextInputIcon(props: TextInputIconProps){
+    return(
+        <Slot className="w-6 h-6 text-gray-400 ">
+            {props.children}
+        </Slot>
+    )
+
+}
+
+TextInputIcon.displayname = 'TextInput.Icon'
+
+export interface TextInputInputProps extends InputHTMLAttributes<HTMLInputElement>{
+}
+
+function TextInputInput (props: TextInputInputProps) {
+    return (
+        <input
+            className = "bg-transparent flex-1 text-gray-100 text-xs placeholder:text-gray-400 outline-none"
+            {...props}
+        />
+    )
+}
+
+export const TextInput = {
+    Root: TextInputRoot,
+    Input: TextInputInput,
+    Icon: TextInputIcon,
+}
+
---
+++ storybook-static/assets/preview.50a3547c.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.fe3c2022.js";import"./iframe.5cd3f514.js";import"./es.map.constructor.d4b24117.js";import"./es.number.to-fixed.28571312.js";import"./index.9e4bfbf3.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.50a3547c.js.map
